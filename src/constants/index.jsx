e33fce207bbed27280ed8066a4f8f56dddf681e9
shanmu <shanmusetty@gmail.com>
2026-06-22 16:05:00 +0530

    Initial Commit

---
+++ src/constants/index.jsx
@@ -0,0 +1,11 @@
+export const navLinks = [
+  {label:'Mac'},
+  {label:'iPad'},
+  {label:'iPhone'},
+  {label:'Watch'},
+  {label:'AirPods'},
+  {label:'TV & Home'}, 
+  {label:'Entertainment'},
+  {label:'Accessories'},
+  {label:'Support'}
+]
